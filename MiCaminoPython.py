91f093dd3f023920d7502aea0a7e1f502ac935c1
Jose Flores Vacio <josefvacio@gmail.com>
2020-07-10 23:14:52 -0700

    Declaracion De Variables Basicas

--- MiCaminoPython.py
+++ MiCaminoPython.py
@@ -1,7 +1,9 @@
-print("Empezando El Lenguaje De Mi Camino")
-print("==================================")
-print("Este Lenguaje Se Puede Usar en Robotica")
-print("==================================")
-print("Intelegencia Artificial")
-print("==================================")
-print("Ciencia De Datos")
+## Al Parecer Python No Es Estrictamente Tipoado
+## Forma De Declarar Un String O Cadena
+nombre = "Jose Luis Flores Vacio"
+## Forma De Declarar Un Entero
+edad = 24
+## Forma De Declarar Decimales
+peso = 102.56
+## Forma De Declarar Un Booleano
+esta_aprendiendo = True
