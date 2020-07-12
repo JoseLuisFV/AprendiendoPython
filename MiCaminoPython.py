7cd991a1b334e4ca05c7840b8d2b808dd6124339
Jose Flores Vacio <josefvacio@gmail.com>
2020-07-11 23:41:05 -0700

    Ejemplo De Como Se ingresan datos desde consola, y ademas de otra forma de colocar las varaibles en una cadena

--- MiCaminoPython.py
+++ MiCaminoPython.py
@@ -1,11 +1,11 @@
-## Al Parecer Python No Es Estrictamente Tipoado
-## Forma De Declarar Un String O Cadena
-nombre = "Jose Luis Flores Vacio"
-## Forma De Declarar Un Entero
-edad = 24
-## Forma De Declarar Decimales
-peso = 102.56
-## Forma De Declarar Un Booleano
-esta_aprendiendo = True
-## Se Concatena con una '+' y para poner otra variable se coloca ','
-print("hi my name is " + nombre + ", what is your name?",edad)
+## Con el input se le puede ingrese
+nombre = input("Cual es tu nombre? ")
+## la funcion int convierte la cadena en un entero
+edad = int(input("Cual es tu edad? "))
+ciudad = input("Donde vives? ")
+email = input("Ingrese un correo electronico que use")
+
+# Existe una manera convertir todoen cadena y es con f
+# al inicicio seguido de ""
+print(f"Hola {nombre} un gusto conocerte sabias que tu {edad}"
+      f" Si le sumas 1 siempre cambia al tipo que es? ")
