c9649bb19e564d6ed10a08f033676a6fb44e3edb
Jose Flores Vacio <josefvacio@gmail.com>
2020-07-12 22:22:13 -0700

    Ejercicio en python de while junto con if del libro

--- MiCaminoPython.py
+++ MiCaminoPython.py
@@ -1,7 +1,27 @@
-x = 0
-# El bucle while asi se declara y lo que sera repetir 11 veces el codigo
-while x <= 10:
-      print(x)
-      x += 1
+# ejercicio while donde el usuario debe adivinar un numero y tiene
+# 3 intentos donde si no adivina obtiene un mensaje de que fallo
+# todas sus oportunidades, la siguiente opcion es que si gana al
+# final le enviemos un mensaje de fuertudo y si gana antes de eso
+# solo recibe felicitaciones
+numero_a_adivinar = 19
+intento = 1
+print("Bienvenido a tu muerte, pero si adivinas se te da otra vida")
+while intento <= 3:
+      numero_adivinado = int(input("Elige cualquier numero y prueba "
+                                    "tu suerte: "))
+      if numero_a_adivinar == numero_adivinado:
+            if intento < 3:
+                  print("Felicidades Ganaste, al parecer tienes talento")
+                  intento = 3
+            else:
+                  print("Eres un ***dito suertudo, Ganaste "
+                        "en ultimo intento")
+      else:
+            if intento < 3:
+                  print("Mala suerte intentalo de nuevo")
+            else:
+                  print("Se te acabaron las oportunidades, no hay vuelta "
+                        "atras a tu destino")
+      intento += 1
 
-print("Fin del programa")
+
