ec523b1fb29e63a62d0612fd7bd46d43932e1add
Jose Flores Vacio <josefvacio@gmail.com>
2020-07-12 21:42:17 -0700

    Forma de como utilizar while en Python

--- MiCaminoPython.py
+++ MiCaminoPython.py
@@ -1,29 +1,7 @@
+x = 0
+# El bucle while asi se declara y lo que sera repetir 11 veces el codigo
+while x <= 10:
+      print(x)
+      x += 1
 
-password = input("Write your new password: ")
-print("welcome again on OnFireNews")
-password_auth = input("Write your password: ")
-# Manera en como se declara la declaracion "if"
-if password == password_auth:
-      print("Congrats you are learning")
-else:
-      print("error password didnt match")
-
-number = int(input("Choose a number between 1 to 3: "))
-
-# Declaracion de un if con mas de dos posibilidades
-if number == 1:
-      print("No hubo suerte ")
-elif number == 2:
-      print("ganaste")
-      other_number = int(input("Pero antes dinos que otro numero hubieras "
-                           "escrio: "))
-      if other_number == 1:
-            print("Excelente ahora si ganaste")
-      elif other_number == 3:
-            print("Mala eleccion se te dara un cuarto del premio")
-      else:
-            print("Estaba hablando de las otras dos opciones baboso")
-elif number == 3:
-      print("No Pienses muy Grande")
-else:
-      print("You dont know read or what")
+print("Fin del programa")
