d1ee54d5543198ba62075d9582a7556c12d89098
Jose Flores Vacio <josefvacio@gmail.com>
2020-07-15 10:58:57 -0700

    Listas en Python

--- MiCaminoPython.py
+++ MiCaminoPython.py
@@ -1,11 +1,34 @@
-# For anidados
-for a in range(3):
-      # imprimer de 0 a 2 en la posicion que se encuentre
-      # la variable a
-      for b in range(3):
-            # imprimer de 0 a 2 en la posicion que se encuentre
-            # la variable b
-            for c in range(3):
-                  #imprimer de 0 a 2 en la posicion que se encuentre
-                  #la variable c
-                  print(f"({a}, {b}, {c})")
+# Este programa abarca listas
+# forma de declarar listas
+videogames_characters = ["Mario", "Sonic", "Master Chief", "Marcus Fenix",
+                         "Cratos", "Cayde-6", "San-14", "Joker Persona 5"]
+#Con la funcion len obtenemos la cantidad de personajes que tiene la lista
+print(len(videogames_characters))
+#Se Elige el primer personaje de la lista
+
+character = videogames_characters[0]
+print(character)
+
+#Se Elige el ultimo personaje de la lista
+character = videogames_characters[-1]
+print(character)
+#Se Elige el ultimo personaje de la lista
+character = videogames_characters[-2]
+print(character)
+#En python tenemos una manera interesante de obtener un conjunto de variables
+#existentes en la lista y eso es utilizando limites de donde las queremos agarrar
+#pero el simbolo que nos ayuda a esto es el ":", el cual del lado izquierdo
+#el limite inferior a la izquierda y el limite superior a la derecha
+
+#el siguiente codigo imprimer el conjuntos de variables que se encuentran a partir
+# de lo posicion 3, eso incluye lo que hay en esta misma posicion
+print(videogames_characters[4:])
+#el siguiente es parecido perso toma las que se encuentran abajo de esta posicion
+# pero en este caso no se incluye la que se encuentra en esata misma
+print(videogames_characters[:4])
+#Por ultimo le podemos dar ambos limites
+print(videogames_characters[2:6])
+
+#podemos modificar una de las variables por otra
+videogames_characters[6] = "Link"
+print(videogames_characters)
