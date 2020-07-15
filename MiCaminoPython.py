bc42af9505c43f8e254639bdbabe0cd4bc95ec78
Jose Flores Vacio <josefvacio@gmail.com>
2020-07-15 10:23:01 -0700

    For anidados con comentarios

--- MiCaminoPython.py
+++ MiCaminoPython.py
@@ -1,5 +1,11 @@
 # For anidados
 for a in range(3):
+      # imprimer de 0 a 2 en la posicion que se encuentre
+      # la variable a
       for b in range(3):
+            # imprimer de 0 a 2 en la posicion que se encuentre
+            # la variable b
             for c in range(3):
+                  #imprimer de 0 a 2 en la posicion que se encuentre
+                  #la variable c
                   print(f"({a}, {b}, {c})")
