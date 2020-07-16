eee95d703f2d272ee97106c5ee8b50493744bf53
Jose Flores Vacio <josefvacio@gmail.com>
2020-07-15 23:26:42 -0700

    Manera de crear funciones en Python

--- MiCaminoPython.py
+++ MiCaminoPython.py
@@ -1,39 +1,31 @@
-# Los diccionarios son como tal cual un conjunto de variables que como
-# objetivo deben de tener una relacion entre si, los diccionarios tienen
-# dentro de ellos varaibles que se componen con par de datos, uno que es
-# como un tipo identificador y el otro sera el valor de interes que queremos
-# guardar
-user = {
-    "nombre": "James",
-    "edad": 30,
-    "Correo electronico": "james@domain.com",
-    "Coche": "Tesla T1"
-}
+#Ejercicio Uno De platzi
 
-# Se accede a este valor escribiendo el valor que lo identifica, pero de la
-# siguiente forma, solo que arrojara un error muy horrible si tal identificador
-# no existe
-print(user["Correo electronico"])
+#Manera de declarar una funcion en Python
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("¿Cuantos pesos" + tipo_pesos + "tienes?: ")
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
 
-# Con el metodo get evitamos este error horrible
-print(user.get("nombre"))
+menu = """
+Bienvenido al conversor de monedas 
 
-# Les podemos dar un nuevo par de valores a diccionario si es
-print(user.get("dob","1,1,1900"))
+1 - Pesos Colombianos
+2 - Pesos Argentinos
+3 - Pesos Mexicanos
 
-# Los diccionarios pueden ser modificables como las listas
-for value in user:
-    if value == "nombre":
-        user[value] = input("Escribe tu nuevo Nombre: ")
-    elif value == "edad":
-        user[value] = int(input("Escribe tu Nueva edad: "))
-    elif value == "Correo electronico":
-        variable_temporal = input("Escribe tu nuevo correo: ")
-        user[value] = variable_temporal
-    elif value == "Coche":
-        variable_temporal = input("Escribe el coche que tienes ahora: ")
-        user[value] = variable_temporal
-    else:
-        print("No se encontro nada")
+Elige una opcion: """
 
-print(user)
+option = int(input(menu))
+
+if option == 1:
+    conversor("Colombianos", 3875)
+elif option == 2:
+    conversor("Argentinos", 65)
+elif option == 3:
+    conversor("Mexicanos", 24)
+
+else:
+    print("Elige una Opcion Correcta porfavor")
