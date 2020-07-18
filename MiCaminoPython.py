072de8a420bdcd6c5dd0990065a723fabe8b9bf9
Jose Flores Vacio <josefvacio@gmail.com>
2020-07-17 17:02:31 -0700

    Segundo ejercicio python platzi

--- MiCaminoPython.py
+++ MiCaminoPython.py
@@ -1,29 +1,25 @@
-# Ejercicio 2 Platzi
-def palindromo(palabra):
-    #La funcion replace que viene ya definida en python y remplaza
-    #los caracteres iguales al primer parametro por caracteres iguales
-    #a los del segundo argumento
-    palabra = palabra.replace(' ', '')
-    #Funcion que pone en minuscula todas las letras
-    palabra = palabra.lower()
-    #Ingresa lo que hay en la variable palabra pero la ingresara
-    #haciendo que la ultima letra de esta variable, sea la primera
-    #para la nueva variable
-    palabra_invertida = palabra[::-1]
-    if palabra == palabra_invertida:
-        return True
-    else:
-        return False
+#Ejercicio Platzi 2
+def es_primo(numero):
+    contador = 0
+
+    for num in range(1, numero + 1):
+        print('numero')
+        if numero == 2:
+            return True
+        if num == 1 or num == numero:
+            continue
+        if numero % num == 0:
+            return False
+    return True
+
 
 
 def main():
-    palabra = input('Escribe una palabra: ')
-    es_palindromo = palindromo(palabra)
-    if es_palindromo == True:
-        print('Es Palindromo')
-
+    numero = int(input('Escribe un numero: '))
+    if es_primo(numero):
+        print('El Numero es primo')
     else:
-        print('No Es Palindromo')
+        print('No es primo')
 
 
 if __name__ == '__main__':
